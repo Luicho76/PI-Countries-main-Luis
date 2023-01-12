0ff0d846dfff483fed95297643719ae6ab5a84d8
Luicho76 <ingenierocivil512@gmail.com>
2023-01-12 13:17:13 -0400

    deploy

--- api/index.js
+++ api/index.js
@@ -20,12 +20,13 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { getAllCountries } = require('./src/controllers/countryController')
+const port = process.env.PORT || 3001;
 
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, async () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(port, async () => {
+    console.log(`%s listening at port ${port}`); // eslint-disable-line no-console
     //await getAllCountries()
   });
 });
